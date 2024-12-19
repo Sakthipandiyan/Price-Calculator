03d499325391286bd9a4d19210afee4e421c5412
Sakthipandiyan <sakthi.jkpm@gmail.com>
2024-12-19 10:26:05 +0530

    clear fix

--- calculate.js
+++ calculate.js
@@ -1,6 +1,7 @@
 function calculate() {
     let price = parseFloat(document.getElementById('price').value);
     let discount = parseFloat(document.getElementById('discount').value);
+    let tax = parseFloat(document.getElementById('tax').value);
 
     if (isNaN(price) || isNaN(discount)) {
         alert('Please enter valid values for price and discount.');
@@ -9,11 +10,20 @@ function calculate() {
 
     let discountAmount = price * discount / 100;
     let taxable_Value = price - discountAmount;
-    let Gst = taxable_Value * 5 / 100;
+    let Gst = taxable_Value * tax / 100;
     let net_Amount = taxable_Value + Gst;
 
-    // Display the result in the result textbox
     document.getElementById('GST').value = Gst.toFixed(2);
     document.getElementById('Taxable Value').value = taxable_Value.toFixed(2);
     document.getElementById('result').value = net_Amount.toFixed(2);
+}
+
+function clearFields() {
+    document.getElementById('price').value = '';
+    document.getElementById('discount').value = '';
+    document.getElementById('tax').value = '';
+
+    document.getElementById('GST').value = '';
+    document.getElementById('Taxable Value').value = '';
+    document.getElementById('result').value = '';
 }
